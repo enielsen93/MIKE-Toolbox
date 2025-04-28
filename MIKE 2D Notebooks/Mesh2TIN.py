fb0323014372cafc4397b08dc090f66d4522db04
Emil Nielsen <enielsen93@hotmail.com>
2025-04-28 07:41:50 +0200

    update to new mikeio syntax

--- MIKE 2D Notebooks/Mesh2TIN.py
+++ MIKE 2D Notebooks/Mesh2TIN.py
@@ -16,7 +16,7 @@ arcpy.env.overwriteOutput = True
 
 arcpy.CheckOutExtension("3D")
 
-mesh_files = [r"\\Aarhusfil\userdata\1101189_Forsyning_og_Klimatilpasning\Bruger\NDKB\Sibu\Catchments_EFGH_V12_01.mesh"]
+mesh_files = [r"C:\Users\elnn\OneDrive - Ramboll\Documents\Aarhus Vand\Jens Juuls Vej\MIKE_URBAN\07_2D\Mesh_v2-00_Z.mesh"]
 
 for mesh_file in mesh_files:
 # mesh_file = r"C:\Papirkurv\MIWM\Herfolge_Flood_endelig_mesh_m_veje_haevet_grunde_interp.mesh"
@@ -28,7 +28,7 @@ for mesh_file in mesh_files:
 
     nodesPath = r"in_memory"
     nodesName = "nodes_Z"
-    arcpy.CreateFeatureclass_management(nodesPath, nodesName, "POINT", has_z = "Enabled", spatial_reference = dfs.projection_string)
+    arcpy.CreateFeatureclass_management(nodesPath, nodesName, "POINT", has_z = "Enabled", spatial_reference = dfs.geometry.projection_string)
     # arcpy.AddField_management(os.path.join(nodesPath, nodesName), "Z", "DOUBLE")
     print("Creating point class")
     with arcpy.da.InsertCursor(os.path.join(nodesPath,nodesName), ["SHAPE@"]) as cursor:
@@ -38,13 +38,13 @@ for mesh_file in mesh_files:
     if clip:
         print("Preparing clip")
         arcpy.CreateFeatureclass_management("in_memory", "ClipPolygon", "POLYGON")
-        boundary_xy_table = dfs.boundary_polylines[1][0].xy
+        boundary_xy_table = dfs.geometry.boundary_polylines[1][0].xy
         polygons = arcpy.Polygon(arcpy.Array([arcpy.Point(xy[0], xy[1]) for xy in boundary_xy_table]))
         with arcpy.da.InsertCursor("in_memory\ClipPolygon", "SHAPE@") as cursor:
             cursor.insertRow([polygons])
 
         arcpy.CreateFeatureclass_management("in_memory", "CutPolygon", "POLYGON")
-        cut_polygons = dfs.boundary_polylines[3]
+        cut_polygons = dfs.geometry.boundary_polylines[3]
         polygons = []
         for cut_polygon in cut_polygons:
             boundary_xy_table = cut_polygon.xy
@@ -54,10 +54,10 @@ for mesh_file in mesh_files:
                 cursor.insertRow([polygon])
 
         print("Creating TIN")
-        arcpy.ddd.CreateTin(mesh_file.replace(".mesh","TIN"), dfs.projection_string,
+        arcpy.ddd.CreateTin(mesh_file.replace(".mesh","TIN"), dfs.geometry.projection_string,
                             r"in_memory\nodes_Z Shape.Z Mass_Points; in_memory\ClipPolygon <None> Hard_Clip; in_memory\CutPolygon <None> Hard_Erase")
     else:
         print("Creating TIN")
-        arcpy.ddd.CreateTin(mesh_file.replace(".mesh","TIN"), dfs.projection_string,
+        arcpy.ddd.CreateTin(mesh_file.replace(".mesh","TIN"), dfs.geometry.projection_string,
                        "%s Shape.Z Mass_Points")
 
